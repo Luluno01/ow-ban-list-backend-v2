1fc453a2467f976b9ac80a111046d5a761d5bfda
Untitled <littleluluno01@163.com>
2019-02-25 04:12:04 +0800

    minor change

--- src/middlewares/store.ts
+++ src/middlewares/store.ts
@@ -2,7 +2,7 @@ import Store from '../helpers/Store'
 import { Router } from '../global'
 import * as Koa from 'koa'
 import * as _log4js from 'koa-log4'
-import { Logger, Log4js } from 'log4js'
+import { Log4js } from 'log4js'
 const log4js: Log4js = _log4js
 
 
